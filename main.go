d8860ee43b1d3e3d49f39e850c8cb4fa6a771ba6
Magesh Chandramouli <mchandramouli@expedia.com>
2019-02-12 11:48:08 -0800

    working ssl codebase

--- main.go
+++ main.go
@@ -11,8 +11,28 @@ import (
 )
 
 func main() {
-	parameters := parse()
+	parameters := parseArguments()
 
+	if mutator, ok := startHttpsServer(parameters); ok {
+		wait()
+		mutator.shutdown(context.Background())
+	} else {
+		os.Exit(1)
+	}
+}
+
+func parseArguments() Parameters {
+	var parameters Parameters
+
+	flag.IntVar(&parameters.port, "port", 443, "server port.")
+	flag.StringVar(&parameters.certFile, "certFile", "/etc/mutator/certs/cert.pem", "File containing tls certificate")
+	flag.StringVar(&parameters.keyFile, "keyFile", "/etc/mutator/certs/key.pem", "File containing tls private key")
+	flag.Parse()
+
+	return parameters
+}
+
+func startHttpsServer(parameters Parameters) (*Mutator, bool) {
 	mutator := &Mutator{
 		props: parameters,
 	}
@@ -23,27 +43,19 @@ func main() {
 	select {
 	case err := <-errs:
 		glog.Errorf("Filed to listen and serve mutator server: %v", err)
-		os.Exit(1)
+		return nil, false
 	case <-time.After(5 * time.Second):
 		glog.Infof("Server listening in port %v", mutator.props.port)
 	}
 
-	// listening OS shutdown singal
+	return mutator, true
+}
+
+func wait() {
+	// subscribe to process shutdown signal
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 	<-signalChan
 
 	glog.Infof("Shutting down initiated")
-	mutator.shutdown(context.Background())
-}
-
-func parse() Parameters {
-	var parameters Parameters
-
-	flag.IntVar(&parameters.port, "port", 443, "Webhook server port.")
-	flag.StringVar(&parameters.certFile, "certFile", "/etc/mutator/certs/cert.pem", "File containing tls certificate")
-	flag.StringVar(&parameters.keyFile, "keyFile", "/etc/mutator/certs/key.pem", "File containing tls private key")
-	flag.Parse()
-
-	return parameters
 }
