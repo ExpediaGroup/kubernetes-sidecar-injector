baff0dd567298c6ec0929f871a1202ae9c599171
Magesh Chandramouli <mchandramouli@expedia.com>
2019-02-16 07:58:43 -0800

    fixing formatting

--- main.go
+++ main.go
@@ -1,88 +1,88 @@
 package main
 
 import (
-	"flag"
-	"github.com/golang/glog"
-	"github.com/mchandramouli/haystack-kube-sidecar-injector/httpd"
-	"github.com/mchandramouli/haystack-kube-sidecar-injector/routes"
-	"os"
-	"os/signal"
-	"syscall"
+    "flag"
+    "github.com/golang/glog"
+    "github.com/mchandramouli/haystack-kube-sidecar-injector/httpd"
+    "github.com/mchandramouli/haystack-kube-sidecar-injector/routes"
+    "os"
+    "os/signal"
+    "syscall"
 )
 
 type config struct {
-	httpdConf         httpd.Conf
-	sideCarConfigFile string
+    httpdConf         httpd.Conf
+    sideCarConfigFile string
 }
 
 func main() {
-	conf := readConfig()
-	simpleServer := httpd.NewSimpleServer(conf.httpdConf)
+    conf := readConfig()
+    simpleServer := httpd.NewSimpleServer(conf.httpdConf)
 
-	var err error
-	defer func() {
-		if err != nil {
-			glog.Errorf("Failed to start server: %v", err)
-			os.Exit(1)
-		}
-	}()
+    var err error
+    defer func() {
+        if err != nil {
+            glog.Errorf("Failed to start server: %v", err)
+            os.Exit(1)
+        }
+    }()
 
-	if err = addRoutes(simpleServer, conf); err != nil {
-		return
-	}
+    if err = addRoutes(simpleServer, conf); err != nil {
+        return
+    }
 
-	if err = startHttpServerAndWait(simpleServer); err != nil {
-		return
-	}
+    if err = startHttpServerAndWait(simpleServer); err != nil {
+        return
+    }
 
-	glog.Infof("Shutting down initiated")
-	simpleServer.Shutdown()
+    glog.Infof("Shutting down initiated")
+    simpleServer.Shutdown()
 }
 
 func addRoutes(simpleServer httpd.SimpleServer, conf config) error {
-	mutator, err := routes.NewMutatorController(conf.sideCarConfigFile)
-	if err != nil {
-		return err
-	}
+    mutator, err := routes.NewMutatorController(conf.sideCarConfigFile)
+    if err != nil {
+        return err
+    }
 
-	simpleServer.AddRoute("/mutate", mutator.Mutate)
-	return nil
+    simpleServer.AddRoute("/mutate", mutator.Mutate)
+    return nil
 }
 
 func readConfig() config {
-	var conf config
+    var conf config
 
-	flag.IntVar(&conf.httpdConf.Port, "port", 443, "server port.")
-	flag.StringVar(&conf.httpdConf.CertFile, "certFile", "/etc/mutator/certs/cert.pem", "File containing tls certificate")
-	flag.StringVar(&conf.httpdConf.KeyFile, "keyFile", "/etc/mutator/certs/key.pem", "File containing tls private key")
-	flag.StringVar(&conf.sideCarConfigFile, "sideCar", "/etc/mutator/sidecar.yaml", "File containing sidecar template")
-	flag.Parse()
+    flag.IntVar(&conf.httpdConf.Port, "port", 443, "server port.")
+    flag.StringVar(&conf.httpdConf.CertFile, "certFile", "/etc/mutator/certs/cert.pem", "File containing tls certificate")
+    flag.StringVar(&conf.httpdConf.KeyFile, "keyFile", "/etc/mutator/certs/key.pem", "File containing tls private key")
+    flag.StringVar(&conf.sideCarConfigFile, "sideCar", "/etc/mutator/sidecar.yaml", "File containing sidecar template")
+    flag.Parse()
 
-	return conf
+    return conf
 }
 
 func startHttpServerAndWait(simpleServer httpd.SimpleServer) error {
-	errC := make(chan error, 1)
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+    errC := make(chan error, 1)
+    signalChan := make(chan os.Signal, 1)
+    signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 
-	defer func() {
-		close(errC)
-		close(signalChan)
-	}()
+    defer func() {
+        close(errC)
+        close(signalChan)
+    }()
 
-	glog.Infof("SimpleServer starting to listen in port %v", simpleServer.Port())
+    glog.Infof("SimpleServer starting to listen in port %v", simpleServer.Port())
 
-	simpleServer.Start(errC)
+    simpleServer.Start(errC)
 
-	// block until an error or signal from os to
-	// terminate the process
-	var retErr error
-	select {
-	case err := <-errC:
-		retErr = err
-	case <-signalChan:
-	}
+    // block until an error or signal from os to
+    // terminate the process
+    var retErr error
+    select {
+    case err := <-errC:
+        retErr = err
+    case <-signalChan:
+    }
 
-	return retErr
+    return retErr
 }
